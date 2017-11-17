8ddc1d69dea9e3a47d293c3ced92e7a7551868be
Dennis Petrillo <dennis.petrillo@yahoo.com>
2017-11-17 12:37:42 -0500

    tests defined, repo file in template populated, resources outlined in setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,3 +3,21 @@
 # Recipe:: setup
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+#template ('/etc/yum.repos.d/mongodb-org-3.4.repo') do
+#  source 'mongodbRepo.erb'
+#end
+
+#package 'mongod' do
+#  package_name 'mongodb-org'
+#  action :install
+#end
+
+#service 'mongod' do
+#  action :start
+#end
+
+#execute "chkconfig mongo on for restart" do
+#  command "sudo chkconfig mongod on"
+#end
+
---
+++ test/smoke/setup/setup_test.rb
@@ -0,0 +1,19 @@
+describe file('/etc/yum.repos.d/mongodb-org-3.4.repo') do
+ it { should exist }
+end
+
+describe file('/etc/yum.repos.d/mongodb-org-3.4.repo') do
+ its ( 'size') { should_not be 0 }
+end
+
+describe package('mongodb-org') do
+  it { should be_installed }
+end
+
+describe service ('mongod') do
+  it { should be_running }
+end
+
+describe file('/var/log/mongodb/mongod.log') do
+  its ('content') { should match /waiting for connections/ }
+end
