449a62db19c5152f4e588e35735878c00af9ba44
Dennis Petrillo <dennis.petrillo@yahoo.com>
2017-11-17 13:09:10 -0500

    copied updated repo to template erb ....ran tests against setup resources in kitchen .... all good

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,20 +4,20 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-#template ('/etc/yum.repos.d/mongodb-org-3.4.repo') do
-#  source 'mongodbRepo.erb'
-#end
+template ('/etc/yum.repos.d/mongodb-org-3.4.repo') do
+  source 'mongodbRepo.erb'
+end
 
-#package 'mongod' do
-#  package_name 'mongodb-org'
-#  action :install
-#end
+package 'mongod' do
+  package_name 'mongodb-org'
+  action :install
+end
 
-#service 'mongod' do
-#  action :start
-#end
+service 'mongod' do
+  action :start
+end
 
-#execute "chkconfig mongo on for restart" do
-#  command "sudo chkconfig mongod on"
-#end
+execute "chkconfig mongo on for restart" do
+  command "sudo chkconfig mongod on"
+end
 
